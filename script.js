fad2a3621467eb4029f9e9d6fb4be58df6c647fa
kiyoko15 <ckiyokos@gmail.com>
2025-04-10 03:09:34 +0900

    js and html refatoration

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-function clickMenu(){
-  if(itens.style.display == "block"){
-    itens.style.display = 'none'
-  }else{
-    itens.style.display = 'block'
+function toggleMenu() {
+  const isOpen = menuIsOpen()
+
+  if (isOpen) {
+    closeMenu()
+  } else {
+    openMenu()
   }
 }
 
+function menuIsOpen() {
+  return itens.classList.contains('itens--active')
+}
 
-document.addEventListener('click', function(event) {
-  const burger = document.getElementById('burger')
-  const itens = document.getElementById('itens')
-  if (itens.style.display === "block" && !burger.contains(event.target) && !itens.contains(event.target)) {
-    itens.style.display = 'none'
+function closeMenu() {
+  itens.classList.remove('itens--active')
+  burger.setAttribute('aria-expanded', false)
+}
+function openMenu() {
+  itens.classList.add('itens--active')
+  burger.setAttribute('aria-expanded', true)
+}
+
+const burger = document.getElementById('burger')
+burger.addEventListener('click', toggleMenu)
+
+const itens = document.getElementById('itens')
+
+document.addEventListener('click', function (event) {
+
+  const isMenuOpen = menuIsOpen()
+
+  const isClickedInsideBurger = burger.contains(event.target)
+  const isclickedInsideMenuItens = !itens.contains(event.target)
+
+  if (isMenuOpen && !isClickedInsideBurger && isclickedInsideMenuItens) {
+    closeMenu()
   }
 })
 
 function handleResize() {
   if (window.innerWidth > 768) {
-      itens.style.display = 'flex'
+    openMenu()
+    burger.removeAttribute('aria-expanded')
+
   } else {
-      if (itens.style.display !== 'block'){
-         itens.style.display = 'none'
-      }
+    closeMenu()
+
   }
 }
 
@@ -30,19 +54,19 @@ window.addEventListener('load', handleResize);
 window.addEventListener('resize', handleResize);
 
 function addCustomerTestimonial() {
-    const testimonialsDiv = document.getElementById('testimonials');
-    const testimonialInput = document.getElementById('testimonialInput');
-    const testimonialText = testimonialInput.value.trim();
-  
-    if (testimonialText !== "") {
-      const newTestimonial = document.createElement('div');
-      newTestimonial.classList.add('testimonial');
-      let costumerName = prompt('Tell us your name')
-      newTestimonial.innerHTML = `<p>"${testimonialText}"</p><p>- ${costumerName}</p>`; // You can add a prompt for the customer's name
-  
-      testimonialsDiv.appendChild(newTestimonial);
-      testimonialInput.value = ""; // Clear the textarea
-    } else {
-      alert("Please enter a testimonial.");
-    }
+  const testimonialsDiv = document.getElementById('testimonials');
+  const testimonialInput = document.getElementById('testimonialInput');
+  const testimonialText = testimonialInput.value.trim();
+
+  if (testimonialText !== "") {
+    const newTestimonial = document.createElement('div');
+    newTestimonial.classList.add('testimonial');
+    let costumerName = prompt('Tell us your name')
+    newTestimonial.innerHTML = `<p>"${testimonialText}"</p><p>- ${costumerName}</p>`; // You can add a prompt for the customer's name
+
+    testimonialsDiv.appendChild(newTestimonial);
+    testimonialInput.value = ""; // Clear the textarea
+  } else {
+    alert("Please enter a testimonial.");
   }
+}
