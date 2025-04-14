98f66ca2e9064a30461408de48e81f1d0bfb6176
kiyoko15 <ckiyokos@gmail.com>
2025-04-14 09:46:06 +0900

    Js refatoration

--- script.js
+++ script.js
@@ -50,23 +50,23 @@ function handleResize() {
 }
 
 // Chame handleResize quando a página carregar e sempre que a janela for redimensionada
-window.addEventListener('load', handleResize);
-window.addEventListener('resize', handleResize);
+window.addEventListener('load', handleResize)
+window.addEventListener('resize', handleResize)
 
 function addCustomerTestimonial() {
-  const testimonialsDiv = document.getElementById('testimonials');
-  const testimonialInput = document.getElementById('testimonialInput');
-  const testimonialText = testimonialInput.value.trim();
+  const testimonialsDiv = document.getElementById('testimonials')
+  const testimonialInput = document.getElementById('testimonialInput')
+  const testimonialText = testimonialInput.value.trim()
 
   if (testimonialText !== "") {
-    const newTestimonial = document.createElement('div');
-    newTestimonial.classList.add('testimonial');
+    const newTestimonial = document.createElement('div')
+    newTestimonial.classList.add('testimonial')
     let costumerName = prompt('Tell us your name')
-    newTestimonial.innerHTML = `<p>"${testimonialText}"</p><p>- ${costumerName}</p>`; // You can add a prompt for the customer's name
+    newTestimonial.innerHTML = `<p>"${testimonialText}"</p><p>- ${costumerName}</p>` 
 
     testimonialsDiv.appendChild(newTestimonial);
-    testimonialInput.value = ""; // Clear the textarea
+    testimonialInput.value = "" // Clear the textarea
   } else {
-    alert("Please enter a testimonial.");
+    alert("Please enter a testimonial.")
   }
 }
